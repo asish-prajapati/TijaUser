c29dbc0e9784886e281ffd37cdae68f247ba4cf4
asish kumar <asishprajapat13@gmail.com>
2021-07-22 18:59:27 +0530

    firebase ntf error resolved

--- App.js
+++ App.js
@@ -59,7 +59,7 @@ function App() {
         'Notification caused app to open from background state:',
         remoteMessage.notification,
       );
-      navigation.navigate(remoteMessage.data.type);
+      // navigation.navigate(remoteMessage.data.type);
     });
 
     // Check whether an initial notification is available
@@ -71,7 +71,7 @@ function App() {
             'Notification caused app to open from quit state:',
             remoteMessage.notification,
           );
-          setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
+          // setInitialRoute(remoteMessage.data.type); // e.g. "Settings"
         }
       });
   }, []);
